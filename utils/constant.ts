20cc8bd74b0d350fed35d04ea758aa5ff47ccada
abdoumes <154231201+Abdou373@users.noreply.github.com>
2024-11-19 22:32:17 +0100

    adding production domain

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,1 +1,8 @@
-export const DOMAIN = "http://localhost:3000";
+const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = "yc-directory-brown-five.vercel.app";
+
+
+
+
+export const DOMAIN = process.env.NODE_ENV === "production" ?
+  PRODUCTION_DOMAIN : DEVELOPMENT_DOMAIN;
