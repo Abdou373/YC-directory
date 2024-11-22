6b9d93464c6ff84d72b5bb77f2fd9f8a04368cf6
abdoumes <154231201+Abdou373@users.noreply.github.com>
2024-11-22 15:46:48 +0100

    improve

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,6 +23,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
   return (
     <>
       <section className="pink_container">
+        <p className="tag">Pitch, Vote and Grow</p>
         <h1 className="heading">Pitch your startup,<br />connect with entrepreneurs</h1>
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Compitation.
